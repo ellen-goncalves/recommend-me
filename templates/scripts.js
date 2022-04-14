9992de8dd2743d906240a0e8ae6bdc2a6c59443b
ellen-goncalves <ellenctgoncalves@gmail.com>
2022-04-14 00:18:38 -0300

    add ideas and ideas form

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,9 +1,26 @@
+function onOff(){
+    document
+        .querySelector("#modal")
+            .classList
+            .toggle("hide")
+    document
+        .querySelector("body")
+        .classList
+        .toogle("hideScroll")
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+
+}
+
 document
     .querySelector("button.fat")
     .addEventListener("click",function(){
         document
-            .querySelector("#footer")
-            .classList
-            .toggle("hide")
+                window.scrollTo({
+                top:0,
+                behavior: 'smooth'
+            })
             
     })
